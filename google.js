4fd5d95659099c6ec7568d6a0e39e6b5c469302b
randallwhitlock <randall.whitlock@live.com>
2018-07-02 18:03:15 -0400

    completed google api function - will need tweaking

---
+++ google.js
@@ -0,0 +1,36 @@
+const language = require('@google-cloud/language');
+
+const client = new language.LanguageServiceClient();
+
+function tweets (tweets) {
+
+const document = {
+  content: tweets, 
+  type: 'PLAIN_TEXT'
+};
+
+  client.annotateText({
+    "document": document,
+    "features": {
+     "classifyText": true,
+     "extractDocumentSentiment": true,
+     "extractEntities": true,
+     "extractEntitySentiment": true,
+     "extractSyntax": true
+    }
+   }
+     )
+  .then(responses => {
+    var response = responses[0];
+      console.log(JSON.stringify(response))
+    // doThingsWith(response)
+  })
+  .catch(err => {
+    console.error(err);
+  });
+
+  client.annotateText({document: document})
+}
+
+module.exports = tweets;
+
