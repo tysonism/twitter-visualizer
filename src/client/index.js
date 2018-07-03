060221372f1d066b2ec965cf9b7af87a71cd2932
Kamry Bowman <kamry.bowman@gmail.com>
2018-07-03 10:51:14 -0600

    Implement hash-router

--- src/client/index.js
+++ src/client/index.js
@@ -2,6 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { HashRouter as Router, Route} from 'react-router-dom';
 /* eslint-disable */
 ReactDOM.render(<Router><Route component={App} /></Router>, document.getElementById('root'));
